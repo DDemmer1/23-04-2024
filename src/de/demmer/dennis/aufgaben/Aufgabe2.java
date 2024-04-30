7e4179dd9a2e245df7812e140ea41ca34a2b2c85
Dennis <dennis.demmer1@gmail.com>
2024-04-30 11:23:55 +0200

    initial commit

--- src/de/demmer/dennis/aufgaben/Aufgabe2.java
+++ src/de/demmer/dennis/aufgaben/Aufgabe2.java
@@ -4,15 +4,26 @@ public class Aufgabe2 {
 	
 	
 	static void reverse(String toReverse) {
+
+		//[ 0, 1 ,2 ]  3
+		//[ A, B, C ]
+		char[] charArr = toReverse.toCharArray();
+
+		for(int i = charArr.length - 1 ;  i >= 0 ; i--) {
+			
+			System.out.print(charArr[i]);
+			
+		}
 		
-	
 		
+
 	}
 	
 	
 	public static void main(String[] args) {
 		
-		reverse("Apfelkuchen");
+		reverse("Java");
+		//nehcuklefpA
 		
 	}
 	
